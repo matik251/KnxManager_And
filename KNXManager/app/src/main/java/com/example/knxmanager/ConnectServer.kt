53311e0511f4358844fcf6e6eceb01a87e1ca626
matik251 <mateusz.pawel.kula@gmail.com>
2021-01-11 03:54:53 +0100

    Dodanie Unsecure Connection

--- KNXManager/app/src/main/java/com/example/knxmanager/ConnectServer.kt
+++ KNXManager/app/src/main/java/com/example/knxmanager/ConnectServer.kt
@@ -1,19 +1,26 @@
 package com.example.knxmanager
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
-import java.util.*
+import java.io.InputStream
+import java.security.KeyStore
+import java.security.SecureRandom
+import java.security.cert.Certificate
+import java.security.cert.CertificateException
+import java.security.cert.CertificateFactory
+import java.security.cert.X509Certificate
+import javax.net.ssl.*
+import kotlin.jvm.Throws
+
 
 class ConnectServer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +38,10 @@ class ConnectServer : AppCompatActivity() {
 
         var urlString : String = "https://" + serverIpET.text + ":" + serverPortET.text
         var srvResponseString = ""
-        CoroutineScope(Dispatchers.IO).launch {
+        //CoroutineScope(Dispatchers.IO).launch {
             serverResponseT.text = srvResponseString
+
+        // Loa
 
             /* Creates an instance of the UserService using a simple Retrofit builder using Moshi
     * as a JSON converter, this will append the endpoints set on the UserService interface
@@ -41,23 +50,65 @@ class ConnectServer : AppCompatActivity() {
             val service = Retrofit.Builder()
                     .baseUrl(urlString)
                     .addConverterFactory(MoshiConverterFactory.create())
+                    .client(getUnsafeOkHttpClient()?.build())
                     .build()
-                    .create(UserService::class.java)
+                    .create(ServerHelloService::class.java)
 
             /* Uses the lifecycle scope to trigger the coroutine. It's important to call this
              * using a scope to follow the structured concurrency principle.
              * https://medium.com/@elizarov/structured-concurrency-722d765aa952
              * https://developer.android.com/topic/libraries/architecture/coroutines */
             lifecycleScope.launch {
-                val users : ServerHello = service.getUsers()
+                try{
+                    val users : Hellos = service.getServerHello()
+                    srvResponseString = users.toString()
+                }catch(e:Exception){
+                    srvResponseString = e.message.toString()
+                }
                 /* This will print the result of the network call to the Logcat. This runs on the
                  * main thread */
-                srvResponseString = users.toString()
                 serverResponseT.text = srvResponseString
             }
+        //}
+    }
+
+
+    fun getUnsafeOkHttpClient(): OkHttpClient.Builder? {
+        return try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts = arrayOf<TrustManager>(
+                    object : X509TrustManager {
+                        @Throws(CertificateException::class)
+                        override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {
+                        }
+
+                        @Throws(CertificateException::class)
+                        override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {
+                        }
+
+                        override fun getAcceptedIssuers(): Array<X509Certificate> {
+                            return arrayOf()
+                        }
+                    }
+            )
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(sslSocketFactory, trustAllCerts[0] as X509TrustManager)
+            builder.hostnameVerifier(object : HostnameVerifier {
+                override fun verify(hostname: String?, session: SSLSession?): Boolean {
+                    return true
+                }
+            })
+            builder
+        } catch (e: java.lang.Exception) {
+            throw RuntimeException(e)
         }
-
-
     }
 }
 
@@ -67,13 +118,14 @@ data class UserResponse(val results: List<User>)
 data class User(val email: String, val phone: String)
 
 
-data class ServerHello(val ID: Int, val HomeText: String?, val ServerTime : Date?)
+data class Hellos(val results : List<ServerHello>)
+data class ServerHello(val id: Int, val homeText: String, val time : String)
 
 
 /* Retrofit service that maps the different endpoints on the API, you'd create one
  * method per endpoint, and use the @Path, @Query and other annotations to customize
  * these at runtime */
-interface UserService {
+interface ServerHelloService {
     @GET("/api")
-    suspend fun getUsers(): ServerHello
+    suspend fun getServerHello(): Hellos
 }
