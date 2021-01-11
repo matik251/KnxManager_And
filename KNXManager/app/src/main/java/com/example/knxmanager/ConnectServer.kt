c0a357012d54c73c0b08f93249dbc8325b958eb7
matik251 <mateusz.pawel.kula@gmail.com>
2021-01-11 06:09:00 +0100

    Dodanie progressbara i fixy

--- KNXManager/app/src/main/java/com/example/knxmanager/ConnectServer.kt
+++ KNXManager/app/src/main/java/com/example/knxmanager/ConnectServer.kt
@@ -2,9 +2,11 @@ package com.example.knxmanager
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.CoroutineScope
@@ -18,13 +20,23 @@ class ConnectServer : AppCompatActivity() {
     lateinit var sharedPrefs : SharedPreferences
     var ipAddress : String = ""
 
+
+    lateinit var serverIpET : EditText
+    lateinit var serverPortET : EditText
+    lateinit var serverResponseT : TextView
+
+    lateinit var conProgBar : ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.connect_server)
         sharedPrefs = getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE)
 
-        var serverIpET = findViewById<EditText>(R.id.editTextTextServerIp)
-        var serverPortET = findViewById<EditText>(R.id.editTextTextServerPort)
+        serverIpET = findViewById<EditText>(R.id.editTextTextServerIp)
+        serverPortET = findViewById<EditText>(R.id.editTextTextServerPort)
+        serverResponseT = findViewById<TextView>(R.id.textView3Response)
+        conProgBar.setVisibility(View.GONE)
+
         var temp = sharedPrefs.getString(PREFERENCE_FILE_KEY, "")
         serverIpET.setText(temp)
         temp = sharedPrefs.getString(PREFERENCE_IP_ADDRESS_PORT, "")
@@ -32,10 +44,7 @@ class ConnectServer : AppCompatActivity() {
     }
 
     fun testServerConnection(view: View) {
-
-        var serverIpET = findViewById<EditText>(R.id.editTextTextServerIp)
-        var serverPortET = findViewById<EditText>(R.id.editTextTextServerPort)
-        var serverResponseT = findViewById<TextView>(R.id.textView3Response)
+        conProgBar.setVisibility(View.VISIBLE)
 
         var urlString: String = "https://" + serverIpET.text + ":" + serverPortET.text
         var srvResponseString = ""
@@ -48,6 +57,7 @@ class ConnectServer : AppCompatActivity() {
             withContext(Dispatchers.Main) {
                 serverResponseT.text = srvResponseString
                 ipAddress = urlString
+                conProgBar.setVisibility(View.GONE)
             }
         }
         serverResponseT.text = srvResponseString
@@ -56,8 +66,6 @@ class ConnectServer : AppCompatActivity() {
     fun saveAndExit(view: View){
         //TODO dodac sprawdzanie czy 200
         var spEdit = sharedPrefs.edit()
-        var serverIpET = findViewById<EditText>(R.id.editTextTextServerIp)
-        var serverPortET = findViewById<EditText>(R.id.editTextTextServerPort)
 
         spEdit.putString(PREFERENCE_IP_ADDRESS_FULL, ipAddress)
         spEdit.putString(PREFERENCE_IP_ADDRESS, serverIpET.text.toString())
